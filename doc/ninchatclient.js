ae04e77711abcad3bd4f0b9ff0c16dde50602c71
Timo Savola <timo@ninchat.com>
2015-09-25 17:37:54 +0300

    fix typo in documentation

--- doc/ninchatclient.js
+++ doc/ninchatclient.js
@@ -8,8 +8,8 @@ NinchatClient = {
 	/**
 	 * Call the sessionless API.  The returned [Promise](#promise) will be
 	 * resolved with an event header array as a parameter to the callback
-	 * functions, or rejected on connection error.  Note that `error` events
-	 * are delivered via the promise's resolve callback, not via the reject
+	 * function, or rejected on connection error.  Note that `error` events are
+	 * delivered via the promise's resolve callback, not via the reject
 	 * callback like when using a Session.  The notify callback is not used.
 	 *
 	 * @see https://ninchat.com/api/v2#sessionless-http-calling
