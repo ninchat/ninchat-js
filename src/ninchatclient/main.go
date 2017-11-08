f28c85e662b9b84158d623daa8329728b3c1d53b
Sami Samuelsson <sami@ninchat.com>
2017-11-08 18:28:08 +0200

    add support to install as npm module

--- src/ninchatclient/main.go
+++ src/ninchatclient/main.go
@@ -13,5 +13,12 @@ const (
 func main() {
 	module := js.Global.Get("Object").New()
 	clientlib.Init(module)
-	js.Global.Set(namespace, module)
+	if js.Module != js.Undefined && js.Module.Get("exports") != js.Undefined {
+		e := js.Module.Get("exports")
+		e.Set("call", module.Get("call"))
+		e.Set("newSession", module.Get("newSession"))
+		e.Set("stringifyFrame", module.Get("stringifyFrame"))
+	} else {
+		js.Global.Set(namespace, module)
+	}
 }
